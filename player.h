7acdca51b2411fd616442ae2ac85d74fb58d7be5
yamadaibuki <hensati11@gmail.com>
2025-06-03 19:40:26 +0900

    アクション関数を作成

--- player.h
+++ player.h
@@ -11,6 +11,11 @@ public:
 
 private:
 	void draw()const;
+	void move();
+	void action();
+	void swing();
+	void tackle();
+	void wind();
 
 	VECTOR	position_;			//座標
 	int		openingUmbrella_;	//モデルハンドル(開いた状態)
