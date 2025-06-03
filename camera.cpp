719d1c74335faa17e9a0b86e3ae0f6428b887bb1
yamadaibuki <hensati11@gmail.com>
2025-06-03 16:01:34 +0900

    カメラの初期座標をコンストラクタで設定

--- camera.cpp
+++ camera.cpp
@@ -3,9 +3,12 @@
 
 Camera::Camera()
 {
-	position_.x = 0.0f;;
-	position_.y = initY;
-	position_.z = initZ;
+	position_		= VGet(0.0f, init_Y, init_z);
+	targetPosition_ = VGet(0.0f, 0.0f, 0.0f);
+
+	//‰œs1`100‚Ü‚Å‚ğƒJƒƒ‰‚Ì•`‰æ”ÍˆÍ‚Æ‚·‚é
+	SetCameraNearFar(1.00f, 100.00f);
+	SetCameraPositionAndTarget_UpVecY(position_, targetPosition_);
 }
 
 Camera::~Camera()
@@ -14,7 +17,5 @@ Camera::~Camera()
 
 void Camera::update()
 {
-	//‰œs1`100‚Ü‚Å‚ğƒJƒƒ‰‚Ì•`‰æ”ÍˆÍ‚Æ‚·‚é
-	SetCameraNearFar(1.00f, 100.00f);
-	SetCameraPositionAndTarget_UpVecY(VGet(position_.x, position_.y, position_.z), VGet(targetPosition_.x, targetPosition_.y, targetPosition_.z));
+	
 }
