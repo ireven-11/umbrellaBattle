0961a7ffe1fdd82c4fa95afaf3a636a35b0cad0c
yamadaibuki <hensati11@gmail.com>
2025-06-06 19:26:17 +0900

    ステージを二重配列に変更および配置

--- stage.cpp
+++ stage.cpp
@@ -3,23 +3,35 @@
 
 Stage::Stage()
 {
-	vanishingCount_ = 0;
-	
 	for (int i = 0; i < tile_number; i++)
 	{
-		position_[i]	= VGet(0.0f + i * 10, 0.0f, 0.0f);
-		canExsist_[i]	= true;
-		modelHandle_[i] = MV1LoadModel("3dmodel/stage/hex_grass.mv1");
-		MV1SetScale(modelHandle_[i], VGet(0.1f, 0.1f, 0.1f));
+		for (int j = 0; j < tile_number; j++)
+		{
+			//奇数列行と偶数行で座標をx座標をずらす
+			if (j % 2 == 0)
+			{
+				position_[j][i] = VGet(init_posX + i * shifting_number, 0.0f, init_posZ + j * (shifting_number));
+			}
+			else
+			{
+				position_[j][i] = VGet(init_posX + i * shifting_number - shifting_number / 2, 0.0f, init_posZ + j * shifting_number);
+			}
+			modelHandle_[j][i] = MV1LoadModel("3dmodel/stage/hex_grass.mv1");
+			MV1SetScale(modelHandle_[j][i], VGet(scale, scale, scale));
+		}
 	}
 
+	reset();
 }
 
 Stage::~Stage()
 {
 	for (int i = 0; i < tile_number; i++)
 	{
-		MV1DeleteModel(modelHandle_[i]);
+		for (int j = 0; j < tile_number; j++)
+		{
+			MV1DeleteModel(modelHandle_[j][i]);
+		}
 	}
 }
 
@@ -27,7 +39,10 @@ void Stage::update()
 {
 	for (int i = 0; i < tile_number; i++)
 	{
-		MV1SetPosition(modelHandle_[i], position_[i]);
+		for (int j = 0; j < tile_number; j++)
+		{
+			MV1SetPosition(modelHandle_[j][i], position_[j][i]);
+		}
 	}
 	draw();
 }
@@ -36,10 +51,26 @@ void Stage::draw()
 {
 	for (int i = 0; i < tile_number; i++)
 	{
-		if (canExsist_)
+		for (int j = 0; j < tile_number; j++)
 		{
-			MV1DrawModel(modelHandle_[i]);
-			DrawFormatString(100, 40 * i+10, GetColor(255, 255, 255), "x:%f,y:%f,z:%f", position_[i].x, position_[i].y, position_[i].z);
+			if (canExsist_[j][i])
+			{
+				MV1DrawModel(modelHandle_[j][i]);
+				//デバッグ用
+				DrawFormatString(100 * i, 40 * j, GetColor(255, 255, 255), "x:%f,y:%f,z:%f", position_[j][i].x, position_[j][i].y, position_[j][i].z);
+			}
+		}
+	}
+}
+
+void Stage::reset()
+{
+	vanishingCount_ = 0;
+	for (int i = 0; i < tile_number; i++)
+	{
+		for (int j = 0; j < tile_number; j++)
+		{
+			canExsist_[j][i] = true;
 		}
 	}
 }
--- stage.h
+++ stage.h
@@ -1,6 +1,10 @@
 #pragma once
 
-constexpr int tile_number = 16;
+constexpr int	tile_number		= 4;
+constexpr float init_posX		= -20.0f;
+constexpr float init_posZ		= 0.0f;
+constexpr float shifting_number	= 10.0f;
+constexpr float scale			= 0.05;
 
 class Stage
 {
@@ -10,10 +14,11 @@ public:
 
 	void update();
 	void draw();
+	void reset();
 
 private:
-	int		modelHandle_[tile_number];	//モデルハンドル
-	VECTOR	position_[tile_number];		//座標
-	bool	canExsist_[tile_number];	//存在できるかフラグ
-	int		vanishingCount_;			//消えるカウント
+	int		modelHandle_[tile_number][tile_number];	//モデルハンドル
+	VECTOR	position_[tile_number][tile_number];	//座標
+	bool	canExsist_[tile_number][tile_number];	//存在できるかフラグ
+	int		vanishingCount_;						//消えるカウント
 };
