9bb473ad0879cc62b062d8b0dc6836c6f1c7a992
yamadaibuki <hensati11@gmail.com>
2025-07-03 15:16:44 +0900

    距離計算関数を作成

---
+++ calculateDistance.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//template<typename T>
+//T CalculateDistance(VECTOR position1, VECTOR position2)
+//{
+//	VECTOR tempVector = VGet(position1.x, position1.y, position1.z);
+//	tempVector = VSub(value, position2);
+//	tempVector = VGet(value.x * value.x, value.y * value.y, value.z * value.z);
+//	T value = tempVector.x + tempVector.y + tempVector.z;
+//	return sqrt(value);
+//}
