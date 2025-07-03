469d87cb9d8a09020c3db88d1ac44e41ada4f87a
yamadaibuki <hensati11@gmail.com>
2025-07-03 19:34:39 +0900

    アクションステートの状態を宣言

--- cpu.h
+++ cpu.h
@@ -8,8 +8,30 @@ public:
 	~CPU();
 
 private:
-
-
 	//s“®‚Ìó‘Ô
 	shared_ptr<StateMachine::IState> actionState_;
+	static shared_ptr<ActionState::WaitState> waitState_()
+	{
+		static shared_ptr<ActionState::WaitState> state = make_shared<ActionState::WaitState>();
+		return state;
+	}
+	static shared_ptr<ActionState::AttackState> attackState_()
+	{
+		static shared_ptr<ActionState::AttackState> state = make_shared<ActionState::AttackState>();
+		return state;
+	}
+	static shared_ptr<ActionState::ChaseState> chaseState_()
+	{
+		static shared_ptr<ActionState::ChaseState> state = make_shared<ActionState::ChaseState>();
+		return state;
+	}static shared_ptr<ActionState::EscapeState> escapeState_()
+	{
+		static shared_ptr<ActionState::EscapeState> state = make_shared<ActionState::EscapeState>();
+		return state;
+	}
+	static shared_ptr<ActionState::ThinkState> thinkState_()
+	{
+		static shared_ptr<ActionState::ThinkState> state = make_shared<ActionState::ThinkState>();
+		return state;
+	}
 };
