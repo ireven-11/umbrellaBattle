b1911751925702e9605f1296d9d79f66d6250e7c
yamadaibuki <hensati11@gmail.com>
2025-07-03 19:33:49 +0900

    エラーの原因を修正

--- calculateDistance.h
+++ calculateDistance.h
@@ -1,11 +1,11 @@
 #pragma once
 
-//template<typename T>
-//T CalculateDistance(VECTOR position1, VECTOR position2)
-//{
-//	VECTOR tempVector = VGet(position1.x, position1.y, position1.z);
-//	tempVector = VSub(value, position2);
-//	tempVector = VGet(value.x * value.x, value.y * value.y, value.z * value.z);
-//	T value = tempVector.x + tempVector.y + tempVector.z;
-//	return sqrt(value);
-//}
+template<typename T>
+T CalculateDistance(VECTOR position1, VECTOR position2)
+{
+	VECTOR tempVector = VGet(position1.x, position1.y, position1.z);
+	tempVector = VSub(tempVector, position2);
+	tempVector = VGet(tempVector.x * tempVector.x, tempVector.y * tempVector.y, tempVector.z * tempVector.z);
+	T value = tempVector.x + tempVector.y + tempVector.z;
+	return sqrt(value);
+}
