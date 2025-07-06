dbc4449fe4fccf816ff75227c06595300fac2fd6
yamadaibuki <hensati11@gmail.com>
2025-07-06 16:50:48 +0900

    updateに処理を追加

--- cpu.cpp
+++ cpu.cpp
@@ -1,4 +1,6 @@
 #include"Dxlib.h"
+#include"charaBase.h"
+#include"cpuBrain.h"
 #include"cpu.h"
 
 /// <summary>
@@ -7,6 +9,7 @@
 /// <param name="join_number">何Pかの数字</param>
 CPU::CPU(const int join_number) : CharaBase(join_number)
 {
+	actionState_ = thinkState_();
 }
 
 /// <summary>
@@ -14,4 +17,25 @@ CPU::CPU(const int join_number) : CharaBase(join_number)
 /// </summary>
 CPU::~CPU()
 {
+}
+
+void CPU::update()
+{
+	//コントローラーの入力状態を取得する
+	GetJoypadDirectInputState(controlerNumber_, &input);
+
+	//状態によって行動を変える
+	actionState_->update(this);
+	state_->update(this);
+
+	//でばっぐリセット
+	if (CheckHitKey(KEY_INPUT_D) == true)
+	{
+		position_.y = 0.0f;
+	}
+
+	MV1SetPosition(openingUmbrella_, position_);
+	MV1SetPosition(closingUmbrella_, position_);
+	MV1SetPosition(fan_, position_);
+	draw();
 }
